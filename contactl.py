36305ebd4165ba8f37bc84fb6f65f072740196c3
Laura Shavia <shavialaura462@gmail.com>
2026-06-19 15:15:34 +0300

    .
    .

--- contactl.py
+++ contactl.py
@@ -1,7 +1,30 @@
 contacts={}
+#adding a contact
 def add_contact(name,phone):
     contacts[name]=phone
     print(f"Contact `{name} added!")
-
+#finding a contact
 def find_contact(name):
     return contacts.get(name,"Contact not found...")
+def list_contact():
+    if not contacts:
+        print("No Contacts yet")
+    for name,phone in contacts.items():
+        print(f"{name}:{phone}")
+#deleting a contact
+def del_contact(name):
+    if name in contacts:
+        del contacts[name]
+        print(f"{name} deleted")
+    else:
+        print("Contact not found")
+
+add_contact("Laura","0106639838")
+add_contact("Shavia","0795901578")
+add_contact("Brian","12345567897")
+#list_contact()
+print(find_contact("Shavia"))
+
+
+
+
