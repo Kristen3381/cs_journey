2109882767fdf14d12c800908dc994fed7d1a773
Laura Shavia <shavialaura462@gmail.com>
2026-06-19 14:10:52 +0300

    ,
    .

---
+++ list.py
@@ -0,0 +1,15 @@
+siblings=["Laura","Daisy","Shavia","Brian","Tobby","Msani","Fofo","Henny","shall","Dorry","Danny"]
+print(siblings[1])
+print(siblings[-1])
+print (siblings[1:4])
+siblings.append("Isaac")
+siblings.insert(0,"Gibbons")
+print(siblings)
+siblings.remove("Shavia")
+print(len(siblings))
+
+nums=[4,6,4,8,9,11,15,1,17]
+print(max(nums))
+print(min(nums))
+print(sum(nums))
+
