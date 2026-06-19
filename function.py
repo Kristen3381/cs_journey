48d1d9e58beca4dd767916a02253f4a1fd584e01
Laura Shavia <shavialaura462@gmail.com>
2026-06-19 13:17:21 +0300

    ..
    ..

---
+++ function.py
@@ -0,0 +1,15 @@
+def greet(name,greeting="Hello"):
+    return f"{greeting},{name}!"
+
+print(greet("Laura"))
+print(greet("Henny","Bonjour"))
+
+def is_even(n):
+    return n%2==0
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+evens=[]
+for n in numbers:
+    if is_even(n):
+        evens.append(n)
+    print(evens)
