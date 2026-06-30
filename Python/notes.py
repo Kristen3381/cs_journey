96b2a9af3ea07a288131cb8c7e8ea43af13f8f58
Laura Shavia <shavialaura462@gmail.com>
2026-06-30 15:16:33 +0300

    .
    .

--- Python/notes.py
+++ Python/notes.py
@@ -1,3 +1,4 @@
+import json
 with open("notes.txt","w") as file:
     file.write("This is my first note.\n")
     file.write("Learning file I/O today.\n")
@@ -6,3 +7,11 @@ with open("notes.txt","r") as file:
     content=file.read()
     print(content)
         
+with open("notes.txt","a") as file:
+    file.write("Add a new note without erasing the previous one \n")
+    print (content)
+
+contacts={
+    "Laura":"010",
+    "Daisy"
+}
